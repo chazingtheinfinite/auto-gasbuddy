9a854ddd1cc432be9b6cffbe83593d9aa831eba8
Jimmy Woo <wooh91@gmail.com>
2019-10-05 19:59:52 -0400

    updated script to collect individual state data

--- scripts/allgas-station-ids.py
+++ scripts/allgas-station-ids.py
@@ -26,24 +26,37 @@ def selenium_based_check(url):
     if 'Oops...' in driver.page_source:
         if VERBOSE: print('FAIL')
     else:
-        # grab content
-        station_count = len(driver.find_elements_by_xpath("//div[@class='wide']//i"))
-        addr_list = driver.find_element_by_xpath("//div[@class='wide']").text.split('\n')
-        
-        if station_count != 0:
-            if VERBOSE: print('Exists!')
-        
-        # extract station name and address
-        for i in range(station_count):
-            station_name = addr_list[(3*i)].split(":")[1].strip()
-            station_addr = addr_list[(3*i+1)].replace(",", " ")
-            res.append(station_name + "," + station_addr)
-    driver.quit()
+        # determine number of subdivisions of city list
+        section_count = len(driver.find_elements_by_xpath("//div[@class='wide']"))
+        section_list = []
 
+        for i in range(1, section_count + 1):
+            # subdivisions of city list
+            section_list.append(driver.find_element_by_xpath("//div[@class='wide'][{}]".format(i)).text.split('\n'))
+        
+        for i in range(len(section_list)):
+            for j in range(len(section_list[i])):
+                driver.get(BASE_QUERY + url + "/" + section_list[i][j].replace(' ', '_'))
+                
+                if VERBOSE: print("Current city: {}".format(section_list[i][j]))
+                
+                # station list and count of current city
+                station_list = driver.find_element_by_xpath("//div[@class='wide']").text.split('\n')
+                station_count = len(driver.find_elements_by_xpath("//div[@class='wide']//i"))
+                
+                for k in range(station_count):
+                    station_name = station_list[(3*k)].split(":")[1].strip()
+                    station_addr = station_list[(3*k+1)].replace(",", " ")
+
+                    if VERBOSE: print("Station Name: {}, Station Address: {}".format(station_name, station_addr))
+
+                    res.append(station_name + "," + station_addr)
+                    driver.back()
+    driver.quit()
     return res
 
 # set current state if resuming progress
-curr_state = 'AL'
+curr_state = 'MI'
 
 if os.path.exists(OUT_FILE):
     if VERBOSE: print("Appending...") 
@@ -59,7 +72,7 @@ curr_index = state_list.index(curr_state)
 # query data
 for i in range(curr_index, len(state_list)):   
     curr_state = state_list[i]
-    station_data = selenium_based_check(state_list[i] + "/all/")
+    station_data = selenium_based_check(state_list[i])
 
     if VERBOSE: print("Current State: {}".format(curr_state + " " + str(i)))
 
