c42ff098c96b434b824d80058e38a3e31fd8041e
Kevin Dick <kevin.dick9@gmail.com>
2020-01-08 09:05:44 -0500

    Sampled a new set of MTurk qualification links...

--- scripts/select-qualifying-urls.py
+++ scripts/select-qualifying-urls.py
@@ -4,14 +4,14 @@ import random
 num_select = 1000
 
 # For Replicability of results...
-random.seed(42)
+random.seed(420)
 
 file_in   = '../data/station-coords/mturk-all-seed-urls.txt'
-file_pre  = '../data/station-coords/mturk-prequal-urls.txt'
-file_post = '../data/station-coords/mturk-postqual-urls.txt'
+file_pre  = '../data/station-coords/mturk-prequal-urls-round2.txt'
+file_post = '../data/station-coords/mturk-postqual-urls-round2.txt'
 
 with open(file_in, 'r') as fin, open(file_pre, 'w') as fpre, open(file_post, 'w') as fpost:
-        all_urls = fin.readlines()
+	all_urls = fin.readlines()
 	random.shuffle(all_urls)
 	# Write out top selection and remainder to post-qualification file...
 	for i in range(num_select):          fpre.write(all_urls[i])
